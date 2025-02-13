8f9e314103a5a771b1c359b9f98d9e5cb05cb4aa
Mohammed Abir <risingabir1@gmail.com>
2025-02-13 14:24:49 +0600

    ss

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebarToggeler = document.querySelector(".sidebar-toggler");
+const projects = document.querySelector("#projects");
 
 sidebarToggeler.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
+});
+
+projects.addEventListener("click", () =>{
+    console.log("clicked");
 });

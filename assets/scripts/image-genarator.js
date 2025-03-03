a4346f7b74f4f06f18c1e2972a041375ac005ddf
Mohammed Abir <risingabir1@gmail.com>
2025-03-03 16:26:46 +0600

    updated

---
+++ assets/scripts/image-genarator.js
@@ -0,0 +1,144 @@
+const themeToggle = document.querySelector(".theme-toggle");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = document.querySelector(".prompt-input");
+const promptBtn = document.querySelector(".prompt-btn");
+const modelSelect = document.getElementById("model-select");
+const countSelect = document.getElementById("count-select");
+const ratioSelect = document.getElementById("ratio-select");
+const gridGallery = document.querySelector(".gallery-grid");
+
+const API_KEY = "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"; // Hugging face api key
+
+const examplePrompt = [
+  "A magic forest with glowing plants and fairy homes among giant mushrooms",
+  "An old steampunk airship floating through golden clouds at sunset",
+  "A future Mars colony with glass domes and gardens against red mountains",
+  "A dragon sleeping on gold coins in a crystal cave",
+  "An underwater kingdom with merpeople and glowing coral buildings",
+  "A floating island with waterfalls pouring into clouds below",
+  "A witch's cottage in fall with magic herbs in the garden",
+  "A robot painting in a sunny studio with art supplies around it",
+  "A magical library with floating glowing books and spiral staircases",
+  "A Japanese shrine during cherry blossom season with lanterns and misty mountains",
+  "A cosmic beach with glowing sand and an aurora in the night sky",
+  "A medieval marketplace with colorful tents and street performers",
+  "A cyberpunk city with neon signs and flying cars at night",
+  "A peaceful bamboo forest with a hidden ancient temple",
+  "A giant turtle carrying a village on its back in the ocean",
+];
+// Sst theme based on saved preferences or system default
+(() => {
+  const savedTheme = localStorage.getItem("theme");
+  const systemPrefersDark = window.matchMedia(
+    "(prefers-color-schema: dark)"
+  ).matches;
+  const isDarkTheme =
+    savedTheme === "dark" || (!savedTheme && systemPrefersDark);
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  themeToggle.querySelector("i").className = isDarkTheme
+    ? "fa-solid fa-sun"
+    : "fa-solid fa-moon";
+})();
+//Switch between light and dark themes
+const toggleTheme = () => {
+  const isDarkTheme = document.body.classList.toggle("dark-theme");
+  themeToggle.querySelector("i").className = isDarkTheme
+    ? "fa-solid fa-sun"
+    : "fa-solid fa-moon";
+};
+const getImageDimensions = (aspectRatio, baseSize = 512) => {
+  const [width, height] = aspectRatio.split("/").map(Number);
+  const scaleFactor = baseSize / Math.sqrt(width * height);
+  let calculateWidth = Math.round(width * scaleFactor);
+  let calculateHeight = Math.round(height * scaleFactor);
+
+  //ensure dimensions are multiplies by 16
+  calculateWidth = Math.floor(calculateWidth / 16) * 16;
+  calculateHeight = Math.floor(calculateHeight / 16) * 16;
+
+  return { width: calculateWidth, height: calculateHeight };
+};
+const updateImageCard = (imgIndex, imgUrl) => {
+  const imgCard = document.getElementById(`img-card-${imgIndex}`);
+  if (!imgCard) return;
+  imgCard.classList.remove("loading");
+  imgCard.innerHTML = `<img src="${imgUrl}" class="result-img" />
+              <div class="image-ovelay">
+                <a href="${imgUrl}" class="img-download-btn" download="${Date.now()}.png">
+                  <i class="fa-solid fa-download"></i>
+                </a>
+              </div>`;
+};
+const generateImages = async (
+  selectedModel,
+  imageCount,
+  aspectRatio,
+  promptText
+) => {
+  const MODEL_URL = `https://api-inference.huggingface.co/models/${selectedModel}`;
+  const { width, height } = getImageDimensions(aspectRatio);
+
+  const imagePromise = Array.from({ length: imageCount }, async (_, i) => {
+    //send request to the API
+    try {
+      const response = await fetch(MODEL_URL, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+          "x-use-cache": "false",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          inputs: promptText,
+          parameters: { width, height },
+        }),
+      });
+      if (!response.ok) throw new Error((await response.json())?.error);
+      const result = await response.blob();
+      updateImageCard(i, URL.createObjectURL(result));
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  await Promise.allSettled(imagePromise);
+};
+//create placeholders cards wuth loading spinners
+const createImageCards = (
+  selectedModel,
+  imageCount,
+  aspectRatio,
+  promptText
+) => {
+  gridGallery.innerHTML = "";
+  for (let i = 0; i < imageCount; i++) {
+    gridGallery.innerHTML += `<div class="img-card loading" id="img-card-${i}" style="aspect-ratio: ${aspectRatio}">
+              <div class="status-conatainer">
+                <div class="spinner"></div>
+                    <i class="fa-solid fa-triangle-exclamtion"></i>
+                    <p class="status-txt">Generating...</p>
+               </div>
+            </div>`;
+  }
+  generateImages(selectedModel, imageCount, aspectRatio, promptText);
+};
+// handle submission
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  // Get form values
+  const selectedModel = modelSelect.value;
+  const imageCount = parseInt(countSelect.value) || 1;
+  const aspectRatio = ratioSelect.value || "1/1";
+  const promptText = promptInput.value.trim();
+
+  createImageCards(selectedModel, imageCount, aspectRatio, promptText);
+};
+
+//Fill prompt with random examples
+promptBtn.addEventListener("click", () => {
+  const prompt =
+    examplePrompt[Math.floor(Math.random() * examplePrompt.length)];
+  promptInput.value = prompt;
+  promptInput.focus();
+});
+promptForm.addEventListener("submit", handleFormSubmit);
+themeToggle.addEventListener("click", toggleTheme);
